b6b3f43c9363a700df441f37cfa8f64e5af9eece
Sean Collings <nightharrier@gmail.com>
2021-04-02 16:34:23 +0200

    22: creating the cli

--- packages/local-api/src/index.ts
+++ packages/local-api/src/index.ts
@@ -1,3 +1,5 @@
-export default () => {
-  console.log('Server is listening');
+export const serve = (port: number, fileName: string, dir: string) => {
+  console.log('serving traffic on port', port);
+  console.log('saving/fetching cells from', fileName);
+  console.log('that file is in dir', dir);
 };
