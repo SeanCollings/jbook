58705685ca309dbd7e6bf75606bbd25c4e40e61b
Sean Collings <nightharrier@gmail.com>
2021-04-05 17:41:52 +0200

    23: adding a local-only api

--- packages/local-client/src/index.tsx
+++ packages/local-client/src/index.tsx
@@ -16,28 +16,3 @@ const App = () => {
 };
 
 ReactDOM.render(<App />, document.querySelector('#root'));
-
-// **********************
-// Testing code snippets
-// **********************
-
-// const App = () => <div>Hi there!</div>;
-// import 'tiny-test-pkg';
-// import 'bulma/css/bulma.css';
-
-/*
-import React from 'react';
-import ReactDOM from 'react-dom';
-ReactDOM.render(<h1>Hi</h1>, document.querySelector('#root'));
-*/
-
-/*
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-const App = () => {
-  return <div>Hello there</div>;
-};
-
-ReactDOM.render(<App />, document.querySelector('#root'));
-*/
